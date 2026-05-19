d03c64ab5d27f682057ae74507d3a8b4036d343f
LuizaVP <luiza.pompermayer@sptech.school>
2026-05-19 14:55:40 -0300

    fix: lowercase package names

--- src/main/java/Controller/CalculadoraController.java
+++ src/main/java/Controller/CalculadoraController.java
@@ -1,6 +1,6 @@
-package Controller;
+package controller;
 
-import Service.CalculadoraService;
+import service.CalculadoraService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
